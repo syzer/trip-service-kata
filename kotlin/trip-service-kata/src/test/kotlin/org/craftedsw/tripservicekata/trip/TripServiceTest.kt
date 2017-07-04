005857f53acb1b82bc4fdd295c51b7642f65ba72
syzer3@gmail.com <syzer3@gmail.com>
2017-07-04 18:38:24 +0200

    fisrt try

--- kotlin/trip-service-kata/src/test/kotlin/org/craftedsw/tripservicekata/trip/TripServiceTest.kt
+++ kotlin/trip-service-kata/src/test/kotlin/org/craftedsw/tripservicekata/trip/TripServiceTest.kt
@@ -1,5 +1,24 @@
 package org.craftedsw.tripservicekata.trip
 
+import org.craftedsw.tripservicekata.TripService_Original
+import org.craftedsw.tripservicekata.exception.CollaboratorCallException
+import org.craftedsw.tripservicekata.user.User
+import org.junit.Test
+
 class TripServiceTest {
 
+    @Test(expected = CollaboratorCallException::class)
+    fun shouldGiveExceptionIfUserIsNotLoggedIn() {
+
+        // given
+        val user = User()
+
+        /**
+         * system under test
+         */
+        val sut = TripService_Original()
+
+        // when
+        sut.getTripsByUser(user = user)
+    }
 }
