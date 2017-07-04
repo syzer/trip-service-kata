4acfb7da4e54982db4ac379434773aca1294b015
syzer3@gmail.com <syzer3@gmail.com>
2017-07-04 19:30:23 +0200

    exit early or guard clause

--- kotlin/trip-service-kata/src/main/kotlin/org/craftedsw/tripservicekata/TripService_Original.kt
+++ kotlin/trip-service-kata/src/main/kotlin/org/craftedsw/tripservicekata/TripService_Original.kt
@@ -6,27 +6,23 @@ import org.craftedsw.tripservicekata.trip.TripDAO
 import org.craftedsw.tripservicekata.user.User
 import org.craftedsw.tripservicekata.user.UserSession
 import java.util.*
+import java.util.Objects.isNull
 
 open class TripService_Original {
 
     fun getTripsByUser(user: User): List<Trip> {
         var tripList: List<Trip> = ArrayList<Trip>()
         val loggedUser: User? = loggedInUser()
-        var isFriend: Boolean = false
-        if (loggedUser != null) {
-            for (friend in user.friends) {
-                if (friend == loggedUser) {
-                    isFriend = true
-                    break
-                }
-            }
-            if (isFriend) {
-                tripList = TripDAO.findTripsByUser(user)
-            }
-            return tripList
-        } else {
+
+        if (isNull(loggedUser)) {
             throw UserNotLoggedInException()
         }
+
+        val isFriend: Boolean = user.friends.contains(loggedUser)
+        if (isFriend) {
+            tripList = TripDAO.findTripsByUser(user)
+        }
+        return tripList
     }
 
     open fun loggedInUser() = UserSession.instance.loggedUser
